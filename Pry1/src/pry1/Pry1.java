605a83832d8c54fe426280e41d44d4b66c34d8bf
ESTUDIANTE <estudiante@ul.edu.co>
2020-02-12 07:37:44 -0500

    Primer ejercicio

---
+++ Pry1/src/pry1/Pry1.java
@@ -0,0 +1,13 @@
+
+package pry1;
+
+public class Pry1 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Grafico ven=new Grafico(); 
+        ven.setVisible(true); 
+    }
+    
+}
